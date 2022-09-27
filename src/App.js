9ee3e57b0a3ef5ad9c2235725f5f892ebd618c67
savesj <jesuslares.20@hotmail.com>
2022-09-26 20:59:42 -0400

    Fetching Data

--- src/App.js
+++ src/App.js
@@ -6,7 +6,21 @@ function App(){
     let [search, setSearch] = useState('')
     let [message, setMessage] = useState('Search for Music!')
     let [data, setData] = useState([])
-
+    
+    useEffect(() => {
+		const fetchData = async () => {
+			document.title = `${search} Music`
+			const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
+			const resData = await response.json()
+			if (resData.results.length > 0) {
+				setData(resData.results)
+			} else {
+				setMessage('Not Found')
+			}
+		}
+		fetchData()
+	}, [search])
+	
     return (
         <div>
             <SearchBar />
