aa2f36c98bf0f97a06af1869821d038b0c1cfff5
savesj <jesuslares.20@hotmail.com>
2022-10-04 23:19:29 -0400

    Fetching Custom Data

--- src/App.js
+++ src/App.js
@@ -7,33 +7,37 @@ function App(){
     let [message, setMessage] = useState('Search for Music!')
     let [data, setData] = useState([])
 
+    const API_URL = 'https://itunes.apple.com/search?term='
+
     useEffect(() => {
-		const fetchData = async () => {
-			document.title = `${search} Music`
-			const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
-			const resData = await response.json()
-			if (resData.results.length > 0) {
-				setData(resData.results)
-			} else {
-				setMessage('Not Found')
-			}
-		}
-		fetchData()
-	}, [search])
-  
-	const handleSearch = (e, term) => {
-    e.preventDefault()
-    setSearch(term)
-}
+	 if(search) {
+            const fetchData = async () => {
+                document.title = `${search} music`
+                const response = await fetch(API_URL + search)
+                const resData = await response.json()
+                if (resData.results.length > 0) {
+                    return setData(resData.results)
+                } else {
+                    return setMessage('Not Found.')
+                }
+            }
+            fetchData()
+        }
+    }, [search])
 
-return (
-    <div>
-        <SearchBar handleSearch = {handleSearch} />
-        {message}
-        <Gallery />
-    </div>
-)
+    const handleSearch = (e, term) => {
+        e.preventDefault()
+        setSearch(term)
+    }
 
+    return (
+        <div>
+            <SearchBar handleSearch={handleSearch} />
+            {message}
+            <Gallery data={data} />
+        </div>
+    )
 }
 
 export default App
+
