5dbc159371b483374764ce32bad573463a99f9b9
savesj <jesuslares.20@hotmail.com>
2022-10-05 00:15:12 -0400

    Assigning SearchContext new values

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 import { DataContext } from './context/DataContext'
+import { SearchContext } from './context/SearchContext'
 
 
 function App(){
     let [search, setSearch] = useState('')
-    let [message, setMessage] = useState('Search for Music!')
     let [data, setData] = useState([])
+    let [message, setMessage] = useState('Search for Music!')
+    let searchInput = useRef('')
 
     const API_URL = 'https://itunes.apple.com/search?term='
 
@@ -32,16 +34,22 @@ function App(){
         setSearch(term)
     }
 
-    return (
-        <div>
-            <SearchBar handleSearch={handleSearch} />
+     return (
+        <div className="App">
+            <SearchContext.Provider value={{
+                term: searchInput,
+                handleSearch: handleSearch
+            }}>
+                <SearchBar />
+            </SearchContext.Provider>
             {message}
-            <DataContext.Provider value={data} >
+            <DataContext.Provider value={data}>
                 <Gallery />
             </DataContext.Provider>
         </div>
     )
 }
+
 
 export default App
 
