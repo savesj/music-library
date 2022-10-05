c48a84ea9f5b7557964d2ad3a814a94a7b01e19e
savesj <jesuslares.20@hotmail.com>
2022-10-05 01:29:14 -0400

    Refactoring and adding Spinner

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import './App.css';
-import { useEffect, useState, Suspense } from 'react'
+import { useState, Suspense, useEffect } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 import Spinner from './components/Spinner'
 import { createResource as fetchData } from './helper'
 
-
 function App() {
-    let [search, setSearch] = useState('')
-    let [message, setMessage] = useState('Search for Music!')
-    let [data, setData] = useState(null)
+  let [searchTerm, setSearchTerm] = useState('')
+  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState(null)
 
-    useEffect(() => {
+  useEffect(() => {
     if (searchTerm) {
       document.title=`${searchTerm} Music`
       console.log(fetchData(searchTerm))
       setData(fetchData(searchTerm))
   }
   }, [searchTerm])
-    
-    const handleSearch = (e, term) => {
-        e.preventDefault()
-        setSearch(term)
-    }
-    const renderGallery = () => {
+
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearchTerm(term)
+  }
+
+  const renderGallery = () => {
     if(data){
       return (
         <Suspense fallback={<Spinner />}>
@@ -35,14 +35,11 @@ function App() {
 
   return (
     <div className="App">
-        <SearchBar handleSearch={handleSearch} />
-        {message}
-        <Suspense fallback={<h1>Loading...</h1>}>
-            // <Gallery data={data} />
-        </Suspense>
+      <SearchBar handleSearch={handleSearch} />
+      {message}
+      {renderGallery()}
     </div>
-)
+  );
 }
 
 export default App;
-
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,14 @@
-import { useContext } from 'react'
-import { SearchContext } from '../context/SearchContext'
+import { useState } from 'react'
 
-function Searchbar() {
-    const {term, handleSearch} = useContext(SearchContext)
+const SearchBar = (props) => {
+    let [searchTerm, setSearchTerm] = useState('')
 
     return (
-        <form >
-            <input ref={term} type="text" placeholder="Enter a search term here"/>
-            <button onClick={(e) => handleSearch(e, term.current.value)}>Submit</button>
-        </form>
+            <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+                <input type="text" placeholder="Search Here" onChange={(e) => setSearchTerm(e.target.value)} />
+                <input type="submit" />
+            </form>
     )
 }
 
-export default Searchbar
+export default SearchBar
