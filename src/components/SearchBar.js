f7eb41c773b57b52a977793129883f511a967b37
savesj <jesuslares.20@hotmail.com>
2022-10-04 23:31:00 -0400

    Updating SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,18 @@
 import { useState } from 'react'
 
 function SearchBar(props){
-    let [searchTerm, setSearchTerm] = useState('')
+    // We can comment out our searchTerm state variable as we are not using it!
+    // let [searchTerm, setSearchTerm] = useState('')
 
     return (
-        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-
-            <input type="text" placeholder="Enter a search term here"
-            onChange={(e) => setSearchTerm(e.target.value)} />
-
-            <input type="submit" />
-
-        </form>
+            <form>
+                <input type="text" placeholder="Search Here"
+                    onChange={
+                        (e) => props.handleSearch(e, e.target.value)
+                    } />
+                <input type="submit" />
+            </form>
     )
 }
 
-
- export default SearchBar
-            
+export default SearchBar
